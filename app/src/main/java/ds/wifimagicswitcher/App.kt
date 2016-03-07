60e7c31bc3252a9e35ad795d7452e95862f34aa9
deviant <lubotin@gmail.com>
2016-03-07 12:49:28 +0200

    disable fabric

--- app/src/main/java/ds/wifimagicswitcher/App.kt
+++ app/src/main/java/ds/wifimagicswitcher/App.kt
@@ -2,6 +2,7 @@ package ds.wifimagicswitcher
 
 import android.app.Application
 import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import ds.wifimagicswitcher.di.inject
 import ds.wifimagicswitcher.prefs.KotlinPrefsManager
 import io.fabric.sdk.android.Fabric
@@ -13,7 +14,8 @@ class App : Application() {
 		instance = this
 		//LeakCanary.install(this)
 		KotlinPrefsManager.init(this, "prefs")
-		Fabric.with(this,Crashlytics())
+		val crashlyticsCore = CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()
+		Fabric.with(this, Crashlytics.Builder().core(crashlyticsCore).build())
 		inject()
 
 	}
